f42e52e6f3e6a2a7c7b4b870223841df6f8287ac
mark30331 <mark30331@gmail.com>
2020-10-13 04:05:45 -0700

    updated

---
+++ lesson4/scripts/preston-4.js
@@ -0,0 +1,22 @@
+function toggleMenu(){
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation');
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+}
+
+window.addEventListener('load', (event)=>{
+  const lu = document.querySelector('#lastupdated');
+  lu.textContent = document.lastModified;
+
+  const cry = document.querySelector('#copyrightyear');
+  cry.textContent = new Date().getFullYear();
+})
+
+window.addEventListener('load', (event)=>{
+    const lu= document.querySelector("#lastupdated");
+    lu.textContent = document.lastModified;
+
+    const cry = document.querySelector("#copyrightyear");
+    cry.textContent = new Date().getFullYear();
+})
