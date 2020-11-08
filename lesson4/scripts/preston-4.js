d06c9491af821bcc2957f3dc8e79c8e86b2e7f8c
mark30331 <mark30331@gmail.com>
2020-11-08 15:57:01 -0700

    updated

--- lesson4/scripts/preston-4.js
+++ lesson4/scripts/preston-4.js
@@ -1,22 +1,20 @@
-function toggleMenu(){
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation');
-
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-}
-
 window.addEventListener('load', (event)=>{
-  const lu = document.querySelector('#lastupdated');
-  lu.textContent = document.lastModified;
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const dateObject = new Date()
+    var currentDate = days[dateObject.getDay()] + ", " + dateObject.getDate() + " " + months[dateObject.getMonth()] + " " + dateObject.getFullYear();
 
-  const cry = document.querySelector('#copyrightyear');
-  cry.textContent = new Date().getFullYear();
-})
-
-window.addEventListener('load', (event)=>{
-    const lu= document.querySelector("#lastupdated");
-    lu.textContent = document.lastModified;
+    const lu = document.querySelector("#lastupdated");
+    lu.textContent = currentDate
 
     const cry = document.querySelector("#copyrightyear");
     cry.textContent = new Date().getFullYear();
+
+    // add code here to run when page loads
+    const hambutton = document.querySelector('.ham');
+    const mainnav = document.querySelector('.navigation')
+    
+    hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+    window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 })
