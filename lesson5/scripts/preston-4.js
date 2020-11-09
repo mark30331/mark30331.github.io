eb64ada8f02f9b1711598c4c42b4624a593188d3
mark30331 <mark30331@gmail.com>
2020-11-09 07:53:11 -0700

    updated

--- lesson5/scripts/preston-4.js
+++ lesson5/scripts/preston-4.js
@@ -1,27 +1,30 @@
-function toggleMenu(){
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation');
-
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-}
-
 window.addEventListener('load', (event)=>{
-  const lu = document.querySelector('#lastupdated');
-  lu.textContent = document.lastModified;
+    // Code for getting the current date.
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const dateObject = new Date()
+    var currentDate = days[dateObject.getDay()] + ", " + dateObject.getDate() + " " + months[dateObject.getMonth()] + " " + dateObject.getFullYear();
 
-  const cry = document.querySelector('#copyrightyear');
-  cry.textContent = new Date().getFullYear();
-})
-
-window.addEventListener('load', (event)=>{
-    const lu= document.querySelector("#lastupdated");
-    lu.textContent = document.lastModified;
+    // Code that puts in the date.
+    const lu = document.querySelector("#lastupdated");
+    lu.textContent = currentDate
 
     const cry = document.querySelector("#copyrightyear");
     cry.textContent = new Date().getFullYear();
-     const a = document.querySelector("#announcement");
-    today = new Date().getDate();
-    if(today==5){
-      a.classList.toggle("hidden");
-      a.style.display = "block";
+
+    // Code for the toggle function on the menu.
+    const hambutton = document.querySelector('.ham');
+    const mainnav = document.querySelector('.navigation')
+    
+    hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+    window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+    // Code for the top banner toggle function.
+    var advert = document.getElementById("topBanner")
+    if (dateObject.getDay() == 5){
+        advert.style.display = "block";
+    } else {
+        advert.style.display = "none";
+    }
 })
